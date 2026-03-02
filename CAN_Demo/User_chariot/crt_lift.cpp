52bc4a857278a0d3daf860c278c2e1c2780be749
clover <2370905113@qq.com>
2026-03-02 16:09:04 +0800

    R2_Code

--- CAN_Demo/User_chariot/crt_lift.cpp
+++ CAN_Demo/User_chariot/crt_lift.cpp
@@ -1,4 +1,5 @@
 #include "crt_lift.h"
+<<<<<<< HEAD
 void Class_Lift::Init()
 {
 	Motor_Lift_Left.Init(&hcan2,Motor_DJI_ID_0x201);
@@ -71,6 +72,277 @@ void Class_Lift::Caliberation()
 
 }
 
+=======
+
+
+/**
+ * @brief 初始化函数
+ */
+void Class_Lift::Init()
+{
+	Motor_Lift_L.Init(&hcan2,Motor_DJI_ID_0x201);
+    Motor_Lift_R.Init(&hcan2,Motor_DJI_ID_0x202);
+    Motor_Move_L.Init(&hcan2,Motor_DJI_ID_0x203);
+    Motor_Move_R.Init(&hcan2,Motor_DJI_ID_0x204);
+
+    //PID初始化
+    Motor_Lift_L.PID_Omega.Init(5.0f, 0.0,0.0);
+    Motor_Lift_R.PID_Omega.Init(5.0f, 0.0,0.0);
+
+    PID_Distance_L.Init(-160.0f, 0.0f, 0.0f, 0.0f, 0.0f, 14.0);
+    PID_Distance_R.Init(-160.0f, 0.0f, 0.0f, 0.0f, 0.0f, 14.0 );
+
+    Motor_Move_L.PID_Omega.Init(0.5,0.0,0.0);
+    Motor_Move_R.PID_Omega.Init(0.5,0.0,0.0);
+}
+
+/*
+* @brief 获取同步带行程
+*/
+
+void Class_Lift::Distance_Caculate()
+{
+    //计算同步带行程 齿数*节距
+    Now_Distance[0] = Motor_Lift_L.Get_Now_Angle()*Angle_to_Distance - Offset[0];
+
+    Now_Distance[1] = Motor_Lift_R.Get_Now_Angle()*Angle_to_Distance - Offset[1];
+}
+
+/*-----------------------校准状态---------------------------*/
+
+/**
+ * @brief 校准
+ */
+
+void Class_Lift::Caliberate()
+{
+    //速度环校准
+    if(!Caliberate_Flag[0])
+    {
+        Motor_Lift_L.Set_Target_Omega(Caliberate_Speed[0]);
+    }
+    if(!Caliberate_Flag[1])
+    {
+        Motor_Lift_R.Set_Target_Omega(Caliberate_Speed[1]);
+    }
+
+    //校准检测
+    if(Math_Abs(Now_Distance[0]) >= Distance_Caliberate && Math_Abs(Motor_Lift_L.Get_Now_Current()) >= Caliberate_Torque[0])
+    {
+        Caliberate_Flag[0] = true;
+    }
+    if(Math_Abs(Now_Distance[1]) >= Distance_Caliberate && Math_Abs(Motor_Lift_R.Get_Now_Current()) >= Caliberate_Torque[1])
+    {
+        Caliberate_Flag[1] = true;
+    }
+
+
+}
+
+/*
+* @brief 获取校准完成标志
+*/
+bool Class_Lift::Is_Caliberate_Finished()
+{
+    return Caliberate_Flag[0] && Caliberate_Flag[1];
+}
+
+/*
+*  @brief 校准注销
+*/
+void Class_Lift::Caliberate_Cancel()
+ {
+    //速度环锁死
+    Motor_Lift_L.Set_Control_Method(Motor_DJI_Control_Method_OMEGA);
+
+    Motor_Lift_R.Set_Control_Method(Motor_DJI_Control_Method_OMEGA);
+
+
+    Motor_Lift_L.Set_Target_Omega(0.0f);
+    
+    Motor_Lift_R.Set_Target_Omega(0.0f);
+
+ }
+
+/*-----------------------上升状态---------------------------*/
+
+/*
+* @brief 上升
+*/
+void Class_Lift::Up()
+{
+    
+
+}
+
+
+/**
+ * @brief 查看上升是否完成
+ */
+bool Class_Lift::Is_Up_Finished()
+{
+    return false;
+}
+
+/**
+ * @brief 上升注销
+ */
+ void Class_Lift::Up_Cancel()
+ {
+    
+ }
+
+
+/*-----------------------移动状态---------------------------*/
+/** 
+* @brief 移动
+*/
+void Class_Lift::Move()
+{
+
+}
+
+/**
+* @brief 查看移动是否完成
+*/
+bool Class_Lift::Is_Move_Finished()
+{
+   return false;
+}
+
+/**
+* @brief 移动注销
+*/
+void Class_Lift::Move_Cancel()
+{
+
+}
+
+
+/*-----------------------下降状态---------------------------*/
+/**
+* @brief 下降
+*/
+void Class_Lift::Down()
+{
+
+}
+
+/**
+* @brief 查看下降是否完成
+*/
+bool Class_Lift::Is_Down_Finished()
+{
+    return false;
+}
+
+
+/**
+* @brief 下降注销
+*/
+
+void Class_Lift::Down_Cancel()
+{
+
+}
+
+
+/*-----------------------定时器回调函数---------------------------*/
+/**
+ * @brief 定时器回调函数
+ */
+ void Class_Lift::TIM_Calculate_PeriodElapsedCallback()
+ {
+    //实时计算同步带行程
+    Distance_Caculate();
+
+    //状态机回调
+    FSM_Lift.Lift_TIM_Status_PeriodElapsedCallback();
+
+    //PID回调函数
+    
+
+ }
+
+ void Class_Lift_FSM::Lift_TIM_Status_PeriodElapsedCallback()
+ {
+    Status[Now_Status_Serial].Count_Time++;
+
+    switch(Lift_Status)
+    {
+        case Lift_Status_INIT :
+        {
+            Lift->Caliberate();
+
+            //查看校准是否完成
+            if(Lift->Is_Caliberate_Finished())
+            {
+                //设置同步带偏移
+                Lift->Set_Offset(Lift->Get_Now_Distance_L(),Lift->Get_Now_Distance_R());
+                
+                Lift->Caliberate_Cancel();
+
+                Lift_Status = Lift_Status_UP;
+                
+                Set_Status(1);
+            }
+        }
+
+        break;
+
+        case Lift_Status_UP :
+        {
+            Lift->Up();
+
+            if(Lift->Is_Up_Finished())
+            {   
+                Lift->Up_Cancel();
+
+                Lift_Status = Lift_Status_MOVING;
+
+                Set_Status(2);
+            }
+        }
+
+        break;
+
+        case Lift_Status_MOVING :
+        {
+            Lift->Move();
+
+            if(Lift->Is_Move_Finished())
+            {
+                Lift->Move_Cancel();
+
+                Lift_Status = Lift_Status_DOWN;
+
+                Set_Status(3);
+            }
+        }
+
+        break;
+        
+        case Lift_Status_DOWN :
+        {
+            Lift->Down();
+
+            if(Lift->Is_Down_Finished())
+            {
+                Lift->Down_Cancel();
+
+                Lift_Status = Lift_Status_UP;
+
+                Set_Status(1);
+            }
+
+        }
+
+        break;
+
+    }
+}
+ 
+>>>>>>> d6d6a17 (R2_Code)
  /**
   * @brief 符号函数
   * 
@@ -87,6 +359,7 @@ void Class_Lift::Caliberation()
         res = -1;
     }
     return res;
+<<<<<<< HEAD
  }
 /**
  * @brief 角度环速度环的逻辑切换
@@ -167,3 +440,6 @@ void Class_Lift::Caliberation()
     Motor_Lift_Left.TIM_Calculate_PeriodElapsedCallback();
 	Motor_Lift_Right.TIM_Calculate_PeriodElapsedCallback();
 }
+=======
+ }
+>>>>>>> d6d6a17 (R2_Code)
--- CAN_Demo/User_chariot/crt_lift.h
+++ CAN_Demo/User_chariot/crt_lift.h
@@ -1,5 +1,6 @@
 #ifndef CRT_LIFT_H
 #define CRT_LIFT_H
+<<<<<<< HEAD
 
 #include "dvc_motor_dji.h"
 enum class LiftState {
@@ -35,10 +36,94 @@ class Class_Lift
 
     void TIM_Calculate_PeriodElapsedCallback();
 
+=======
+/*---------------------includes-------------------------*/
+#include "dvc_motor_dji.h"
+#include "alg_fsm.h"
+#include "dvc_dr16.h"
+/*---------------------变量声明-------------------------*/
+class Class_Lift;
+/*---------------------枚举-------------------------*/
+
+/**
+ * @brief 抬升状态
+ * 
+ */
+
+enum Enum_Lift_Status
+{
+    Lift_Status_INIT = 0,   //初始状态
+    Lift_Status_UP,         //上升状态
+    Lift_Status_MOVING,     //导轮移动状态
+    Lift_Status_DOWN,       //下降状态
+};
+
+
+/**
+ * @brief 抬升状态机
+ * 
+ */
+
+class Class_Lift_FSM : public Class_FSM
+{
+    public:
+    
+    Class_Lift *Lift;
+
+    Class_DR16 *DR16;
+
+    void Lift_TIM_Status_PeriodElapsedCallback();
+
+    Enum_Lift_Status Lift_Status =  Lift_Status_INIT;
+};
+
+class Class_Lift
+{ 
+    public:
+    //抬升状态机
+    Class_Lift_FSM FSM_Lift;
+    
+    //行程环
+    Class_PID PID_Distance_L;
+    Class_PID PID_Distance_R;
+    
+    //3508同步带抬升
+    Class_Motor_DJI_C620 Motor_Lift_L;
+    Class_Motor_DJI_C620 Motor_Lift_R;
+
+    //2006导轮
+    Class_Motor_DJI_C610 Motor_Move_L;
+    Class_Motor_DJI_C610 Motor_Move_R;
+
+
+/*-----------------------全流程函数---------------------------*/
+    //行程计算
+    void Distance_Caculate();
+
+    void TIM_Calculate_PeriodElapsedCallback();
+
+/*-----------------------状态函数---------------------------*/
+    void Init();
+
+    //校准
+    void Caliberate();
+    
+    //上升
+    void Up();
+
+    //移动
+    void Move();
+
+    //下降
+    void Down();
+
+/*-----------------------接口---------------------------*/
+>>>>>>> d6d6a17 (R2_Code)
     inline void Set_Target_Distance(float Distance_Limit);
 
     inline float Get_Target_Distance_Limit();
 
+<<<<<<< HEAD
     inline float Get_Now_Distance_Left();
     inline float Get_Now_Distance_Right();
     inline float Get_Target_Distance_Left();
@@ -62,11 +147,61 @@ class Class_Lift
     float Target_Distance_Limit = 0.40f;
     //
     float Switch_Distance = 0.02;
+=======
+    //获取左侧同步带当前及目标距离
+    inline float Get_Now_Distance_L();
+
+    inline float Get_Target_Distance_L();
+    
+    //获取右侧同步带当前及目标距离
+    inline float Get_Now_Distance_R();
+
+    inline float Get_Target_Distance_R();
+
+    //是否校准完成
+    bool Is_Caliberate_Finished();
+
+    //是否上升完成
+    bool Is_Up_Finished();
+
+    //是否导轮移动完成
+    bool Is_Move_Finished();
+
+    //是否下降完成
+    bool Is_Down_Finished();
+
+/*-----------------------状态注销---------------------------*/
+    //校准状态注销
+    void Caliberate_Cancel();    
+
+    //上升状态注销
+    void Up_Cancel();
+    
+    //移动状态注销
+    void Move_Cancel();
+
+    //下降状态注销
+
+    void Down_Cancel();
+
+    //设置同步带校准偏差
+    inline void Set_Offset(float __offset_l, float __offset_r);
+
+    private:
+
+/*-----------------------电控参数---------------------------*/  
+    //同步带行程
+    float Now_Distance[2] = { 0.0f, 0.0f };
+    float Target_Distance[2] = { 0.0f, 0.0f };
+
+    float Target_Distance_Limit = 0.40f;
+>>>>>>> d6d6a17 (R2_Code)
     
     //校准距离 贴地时剩余行程3cm
     float Distance_Caliberate = 0.03f;
 
     //校准标志位
+<<<<<<< HEAD
     bool Left_Caliberate_Flag = false;
     bool Right_Caliberate_Flag = false;
     //校准速度
@@ -76,6 +211,19 @@ class Class_Lift
     float Left_Caliberate_Torque = 6.5f;
     float Right_Caliberate_Torque = 6.5f;
 
+=======
+    bool Caliberate_Flag[2] = { false, false };
+
+    //校准速度
+    float Caliberate_Speed[2] = { -5.0f, -5.0f };
+    //校准力矩
+    float Caliberate_Torque[2] = { 6.5f, 6.5f };
+    //偏移量
+    float Offset[2] = { 0.0f, 0.0f };
+
+
+/*-----------------------机械参数---------------------------*/
+>>>>>>> d6d6a17 (R2_Code)
     //齿轮减速比
     float Gearbox_Rate = 1.5;
     //同步带轮直径
@@ -87,6 +235,7 @@ class Class_Lift
     //同步带节距
     float Step = 0.005;  
     //角度转换为同步带行程
+<<<<<<< HEAD
     float Angle_to_Distance = Belt_Sign*Tooth_Number*Step/(2*PI*Gearbox_Rate);
 
     float Left_Offset = 0.0f; 
@@ -101,11 +250,34 @@ inline void Class_Lift::Set_Target_Distance(float __distance)
     Target_Distance_Right = __distance;
 }
 
+=======
+    float Angle_to_Distance = Belt_Sign * Tooth_Number * Step / ( 2 * PI * Gearbox_Rate);
+
+};
+
+/*
+* @brief 设置目标距离
+*
+*/
+
+inline void Class_Lift::Set_Target_Distance(float __distance)
+{
+    Target_Distance[0] = __distance;
+    Target_Distance[1] = __distance;
+}
+
+/*
+* @brief 获取目标距离
+*
+*/
+
+>>>>>>> d6d6a17 (R2_Code)
 inline float Class_Lift::Get_Target_Distance_Limit()
 {
     return Target_Distance_Limit;
 }
 
+<<<<<<< HEAD
 inline bool Class_Lift::Get_Caliberate_Flag()
 {
     return (Left_Caliberate_Flag&&Right_Caliberate_Flag);
@@ -128,4 +300,50 @@ Class_Lift::Get_Target_Distance_Right()
 {
     return Target_Distance_Right;
 }
+=======
+
+
+/*
+* @brief 获取左侧同步带当前距离
+*/
+
+inline float Class_Lift::Get_Now_Distance_L()
+{
+    return Now_Distance[0];
+}
+
+/*
+* @brief 获取右侧同步带当前距离
+*/
+
+inline float Class_Lift::Get_Now_Distance_R()
+{
+    return Now_Distance[1];
+}
+
+/*
+* @brief 获取左侧同步带目标距离
+*/
+
+inline float Class_Lift::Get_Target_Distance_L()
+{
+    return Target_Distance[0];
+}
+inline float 
+Class_Lift::Get_Target_Distance_R()
+{
+    return Target_Distance[1];
+}
+
+
+/*
+* @brief 设置同步带校准偏差
+*/
+inline void Class_Lift::Set_Offset(float __offset_l, float __offset_r)
+{
+    Offset[0] = __offset_l;
+    Offset[1] = __offset_r;
+}
+
+>>>>>>> d6d6a17 (R2_Code)
 #endif //  CRT_LIFT_H
